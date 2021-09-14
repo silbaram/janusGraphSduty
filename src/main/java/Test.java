8314111f1b20fafa238a552f94a568880afb45cd
박상진 <silbaram@wemakeprice.com>
2021-09-14 21:21:38 +0900

    기초적인 내용 테스트 소스 추가

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,29 +1,34 @@
 import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 
+import java.io.IOException;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.janusgraph.core.JanusGraph;
-import org.janusgraph.core.JanusGraphFactory;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 
 public class Test {
-    public static void main(String[] args) {
-//        GraphTraversalSource g = null;
-//        try {
-//            g = traversal().withRemote("conf/remote-graph.properties");
-//            Object age = g.V().has("name", "hercules").values("age").next();
-//            System.out.println("age : " + age);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (g != null) {
-//                    g.close();
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
+    public static void main(String[] args) throws ConfigurationException, IOException {
+        GraphTraversalSource g = null;
+        try {
+            g = traversal().withRemote("conf/remote-graph.properties");
+            Object age = g.V().has("name", "hercules").values("age").next();
+            System.out.println("age : " + age);
 
-        JanusGraph g = JanusGraphFactory.open("conf/jgex-cql.properties");
-        g.openManagement();
+            g.addV("demigod").property("name", "hercules2").iterate();
+            Vertex vertex = g.V().has("name", "hercules2").next();
+            System.out.printf("vertex.id() : " + vertex.id());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (g != null) {
+                    g.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+//        JanusGraph g = JanusGraphFactory.open(ConfigurationUtil.loadPropertiesConfig("conf/remote-graph.properties"));
+//        g.openManagement();
     }
 }
